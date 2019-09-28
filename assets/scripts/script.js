15bdbd95fc33518870c18536bba01ca89830fa06
Damian Ruiz <nrt.damian@gmail.com>
2019-09-27 22:02:36 -0400

    index, style and script updated
    responsive design implemented, copy functionality added and editorial fixes made to index and script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,27 +21,27 @@ function passwordGenerator() {
     let baseStringPassword = " ";
     let i = 0;
 
-    window.alert("Now you will select the type of characters you want in your password. Please select at least one. Click OK to continue");
+    window.alert("Now you will select what type of characters you want in your password. Please select at least one. Click OK to continue");
     while (i === 0) {
-        typeLower = window.confirm("Do you want your password to cotain lower case characters?");
+        typeLower = window.confirm("Do you want your password to contain lower case characters?");
         if (typeLower) {
             baseStringPassword+=lowerCaseChars;
             i+=1;
             //console.log(baseStringPassword);
         }
-        typeUpper = window.confirm("Do you want your password to cotain lower case characters?");
+        typeUpper = window.confirm("Do you want your password to contain upper case characters?");
         if (typeUpper) {
             baseStringPassword+=upperCaseChars;
             i+=1;
             //console.log(baseStringPassword);
         }
-        typeNumbers = window.confirm("Do you want your password to cotain numbers?");
+        typeNumbers = window.confirm("Do you want your password to contain numbers?");
         if (typeNumbers) {
             baseStringPassword+=numbers;
             i+=1;
             //console.log(baseStringPassword);
         }
-        typeSpecial = window.confirm("Do you want your password to cotain special characters?");
+        typeSpecial = window.confirm("Do you want your password to contain special characters?");
         if (typeSpecial) {
             baseStringPassword+=specialChars;
             i+=1;
@@ -54,21 +54,28 @@ function passwordGenerator() {
         //console.log(baseStringPassword);
     }
 
-    let min = 1;
     let max = baseStringPassword.length - 1;
     let randomNumber = Math.random();
-    let index = Math.floor(randomNumber * (max - min) + min);
+    let index = Math.floor(randomNumber * max);
     let password = baseStringPassword.charAt(index);
 
     for (let i = 1; i < passwordLength; i++) {
         randomNumber = Math.random();
-        index = Math.floor(randomNumber * (max - min) + min);
+        index = Math.floor(randomNumber * max);
         console.log(index);
         password+= baseStringPassword.charAt(index);
         //console.log(baseStringPassword[index]);
     } 
 
-    document.getElementById("password").innerHTML=password;
+    document.getElementById("password-area").innerHTML=password;
 
     return console.log(password);
+}
+
+function copy() {
+    let copyPassword = document.getElementById("password-area");
+    copyPassword.select();
+    copyPassword.setSelectionRange(0, 9999);
+    document.execCommand("copy");
+    window.alert("Password copied to clipboard");
 }
