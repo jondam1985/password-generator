1dc573e10893981e1a89849b841c5d9c90d5aa96
Damian Ruiz <nrt.damian@gmail.com>
2019-10-02 22:27:23 -0400

    index, styles, sctipt updated
    Index: classes and Id changed to camelCase, script moved to the bottom of the code. Style: classes and Ids changed to camelCase. Script: code separated in multiple functions.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,26 @@
-function passwordGenerator() {
+//This function records the number of characters selected by the user.
+//It validates that value is a positive integer between 0 and 128 inclusive.
 
-    let passwordLength;
-
+const numChars = () => {
+    let passwordLength = parseInt(window.prompt("Please select the number of characters for your password. It must be between 8 and 128"));
+    if (passwordLength === null || (typeof passwordLength != "number") || passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) { 
+        window.alert("Value not valid, please enter a number between 8 and 128. Click OK to try again");
+    }
+    //console.log(passwordLength);
     while (passwordLength === null || (typeof passwordLength != "number") || passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
         passwordLength = parseInt(window.prompt("Please select the number of characters for your password. It must be between 8 and 128"));
-        console.log(passwordLength);
+        //console.log(passwordLength);
         if (passwordLength === null || (typeof passwordLength != "number") || passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) { 
             window.alert("Value not valid, please enter a number between 8 and 128. Click OK to try again");
         }
     }
+    //console.log(passwordLength);
+    return passwordLength;
+}
 
+//This function generates the string of characters that will be used to generate the password given the user's choices
+
+const typeOfChars = () => {
     let lowerCaseChars = "abcdefghijklmnopqrstuv";
     let upperCaseChars = lowerCaseChars.toUpperCase();
     let numbers = "0123456789";
@@ -53,32 +64,63 @@ function passwordGenerator() {
         baseStringPassword = baseStringPassword.trimStart();
         //console.log(baseStringPassword);
     }
-
-    let max = baseStringPassword.length - 1;
-    let randomNumber = Math.random();
-    let index = Math.floor(randomNumber * max);
-    let password = baseStringPassword.charAt(index);
-
-    for (let i = 1; i < passwordLength; i++) {
-        randomNumber = Math.random();
-        index = Math.floor(randomNumber * max);
-        console.log(index);
-        password+= baseStringPassword.charAt(index);
-        //console.log(baseStringPassword[index]);
-    } 
-
-    document.getElementById("password-area").innerHTML=password;
-
-    let copyButton = document.getElementById("copy");
-    copyButton.disabled = false;
-
-    return console.log(password);
+    return baseStringPassword;
 }
 
-function copy() {
-    let copyPassword = document.getElementById("password-area");
+//This function generates a random number between 0 and a given number
+
+const randomNumber = (number) => {
+    let randomResult = Math.floor(Math.random() * number);
+    return randomResult;
+}
+
+//This function generates a random password given the values passed by the user. 
+//The pasword is presented in the text area.
+//The COPY button becomes active after the password has been generated.
+
+const passwordGenerator = (length, characters) => {
+    let max = characters.length;
+    let random = randomNumber(max);
+    let password = characters.charAt(random);
+    //console.log(random);
+    for (let i = 1; i < length; i++) {
+        random = randomNumber(max);
+        password+=characters.charAt(random);
+        //console.log(random);
+    }
+
+    return password;
+}
+
+//This function collects the user input and generates a random password.
+//It writes the generated password in the text area.
+//The copy button becomes active.
+const generatePassword = () => {
+    let length = numChars();
+    //console.log(length);
+    let characters = typeOfChars();
+    //console.log(characters);
+    let generatedPassword = passwordGenerator(length, characters);
+    //console.log(length);
+    //console.log(characters);
+    //console.log(generatedPassword);
+
+    document.getElementById("passwordArea").innerHTML=generatedPassword;
+
+    let copyButton = document.getElementById("copyButton");
+    copyButton.disabled = false;
+
+    return generatedPassword;
+}
+
+//This funciton copies the contect of the text area (password area) to the clipboard.
+const copy = () => {
+    let copyPassword = document.getElementById("passwordArea");
     copyPassword.select();
     copyPassword.setSelectionRange(0, 9999);
     document.execCommand("copy");
     window.alert("Password copied to clipboard");
 }
+
+document.getElementById("passwordButton").addEventListener("click", generatePassword);
+document.getElementById("copyButton").addEventListener("click", copy);
